89bd74d57d692089cc74a29b82f9be1dffce6028
System Administrator <root@Yusufs-MacBook-Air.local>
2022-07-22 16:55:13 +0300

    RandomPassword Python code

---
+++ RandomPassword.py
@@ -0,0 +1,10 @@
+import random
+character = "abcdefghklmnopqrstuvwxyzABCGHKLMNOPQRSTUVWXYZ1234567890!#$&"
+password_length = int(input("Şifre kaç karakter olsun : "))
+password_count = int(input("Kaç adet şifre olsun : "))
+for x in range(0, password_count):
+        password = ""
+        for x in range(0, password_length):
+            password_char = random.choice(character)
+            password      = password + password_char
+        print("Rastgele Oluşturulan Şifreniz : " , password)
